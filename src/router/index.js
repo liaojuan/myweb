e9d44472462bb69072c6d56ea0451ce2b8b1f1f2
liaoruiwen <1042433140@qq.com>
2020-09-11 15:55:08 +0800

    修改一个小问题

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/page/home/Home.vue'
 import Dashboard from '@/page/home/dashboard/Dashboard.vue'
-import Article from '@/page/home/dashboard/Dashboard.vue'
+import Article from '@/page/home/article/Article.vue'
 import Login from '@/page/login/TheLogin.vue'
 
 Vue.use(Router)
@@ -11,8 +11,13 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: 'dashboard',
       name: 'Home',
-      component: Home
+      component: Home,
+      children: [
+        { path: 'dashboard', name: '首页', component: Dashboard },
+        { path: 'article', name: '文章', component: Article }
+      ]
     },
     {
       path: '/login',
