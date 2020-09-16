2604238cb1bc754e4b83fd26993d752262c85dba
liaoruiwen <1042433140@qq.com>
2020-09-16 14:18:46 +0800

    采用tab 和按钮来，但是下面布局也跟着变了，不对

--- src/page/pricelist/PriceList.vue
+++ src/page/pricelist/PriceList.vue
@@ -1,8 +1,19 @@
 <template>
-  <el-menu mode="horizontal" :default-active="activeIndex" @select="handleselect">
-    <el-menu-item index="1">计价规则</el-menu-item>
-    <el-menu-item index="2">取消规则</el-menu-item>
-  </el-menu>
+  <el-row>
+    <el-col :span="16">
+      <el-tabs v-model="activeIndex" @tab-click="handleClick">
+        <el-tab-pane label="计价规则" name="first">
+          <billing-rules></billing-rules>
+        </el-tab-pane>
+        <el-tab-pane label="取消规则" name="second">
+          <cancel-rules></cancel-rules>
+        </el-tab-pane>
+      </el-tabs>
+    </el-col>
+    <el-col :span="4">
+      <el-button type="primary" round>主要按钮</el-button>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -20,12 +31,12 @@ export default {
   },
   data () {
     return {
-      activeIndex: '1'
+      activeIndex: 'first'
     }
   },
   methods: {
-    handleselect (key, keyPath) {
-      console.log(key, keyPath);
+    handleClick (tab, event) {
+      console.log(tab, event);
     }
   }
 }
